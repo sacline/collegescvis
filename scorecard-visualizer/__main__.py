1fc1566ef0c5f884ac21d2a9ec8f90f9fc31db2d
Stephen Cline <stephenacline13@gmail.com>
2016-08-02 23:38:13 -0500

    Update default database file location

--- scorecard-visualizer/__main__.py
+++ scorecard-visualizer/__main__.py
@@ -22,14 +22,16 @@ def main():
         print('Generating data type file from raw data...')
         decoder.write_data_types(glob_path, types_dest_path)
 
-    db_path = 'college-scorecard.sqlite'
+    db_path = os.path.join(
+        os.path.dirname(__file__), os.pardir, 'data', 'database',
+        'college-scorecard.sqlite')
+    print(db_path)
     if os.path.isfile(db_path):
         print('Database found.')
     else:
         print('Generating database from raw data...')
         start_time = time.time()
-        dbbuilder.initialize_database(
-            'college-scorecard.sqlite', types_dest_path)
+        dbbuilder.initialize_database(db_path, types_dest_path)
         dbbuilder.build_database()
         print('Database generated in %s seconds.' % (time.time() - start_time))
 
