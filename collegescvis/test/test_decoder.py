d0049560412d31a81bfcafe4eb9c35658cd5c9a6
Stephen Cline <stephenacline13@gmail.com>
2016-08-29 19:22:38 -0500

    Improve decoder test docs

--- collegescvis/test/test_decoder.py
+++ collegescvis/test/test_decoder.py
@@ -1,12 +1,24 @@
-"""Unit tests for the decoder module."""
+"""Unit tests for the decoder module.
+
+Classes:
+    TestDataPaths(unittest.TestCase): Tests for raw data path processing.
+    TestReadValues(unittest.TestCase): Tests for read_values function.
+    TestFindType(unittest.TestCase): Tests for find_type function.
+"""
 import unittest
 import decoder
 
+
 class TestDataPaths(unittest.TestCase):
-    """Contains tests related to the raw data path processing."""
+    """Contains tests for raw data path processing.
+
+    Methods:
+        test_empty_path(self): Test an empty data path input.
+        test_nonstring_path(self): Test a non-string data path input.
+    """
 
     def test_empty_path(self):
-        """Tests an empty data path.
+        """Test an empty data path input.
 
         The glob function used in decoder will return an empty list when an
         empty string is passed to it. This test simulates any case in which
@@ -18,7 +30,7 @@ class TestDataPaths(unittest.TestCase):
             FileNotFoundError, lambda: decoder.validate_data_path(empty_path))
 
     def test_nonstring_path(self):
-        """Tests non-string data file paths.
+        """Test non-string data file paths.
 
         Non-string paths should raise a TypeError in the decoder validation
         function.
@@ -31,11 +43,17 @@ class TestDataPaths(unittest.TestCase):
         self.assertRaises(
             TypeError, lambda: decoder.validate_data_path(nonstring_list_path))
 
+
 class TestReadValues(unittest.TestCase):
-    """Contains tests for the decoder module's read_values function."""
+    """Contains tests for the decoder module's read_values function.
+
+    Methods:
+        test_valid_entry(self): Test valid input to read_values.
+        test_invalid_entry(self): Test invalid input to read_values.
+    """
 
     def test_valid_entry(self):
-        """Tests valid entries (arguments) to read_values."""
+        """Test valid entries (arguments) to read_values."""
         valid_entry = ['Category', '5', '8']
         valid_counts = [2, 0, 0]
         self.assertEqual(decoder.read_values(valid_entry), valid_counts)
@@ -45,7 +63,7 @@ class TestReadValues(unittest.TestCase):
         self.assertEqual(decoder.read_values(valid_entry), valid_counts)
 
     def test_invalid_entry(self):
-        """Tests invalid entries (arguments) to read_values."""
+        """Test invalid entries (arguments) to read_values."""
         invalid_int_entry = [3, 5]
         self.assertRaises(
             TypeError, lambda: decoder.read_values(invalid_int_entry))
@@ -62,26 +80,34 @@ class TestReadValues(unittest.TestCase):
         self.assertRaises(
             TypeError, lambda: decoder.read_values(invalid_str_entry))
 
+
 class TestFindType(unittest.TestCase):
-    """Contains tests for decoder find_type method."""
+    """Contains tests for decoder find_type function.
+
+    Methods:
+        test_integer_entry(self): Test an entry containing integer data.
+        test_real_entry(self): Test an entry containing real number data.
+        test_text_entry(self): Test an entry containing text data.
+    """
 
     def test_integer_entry(self):
-        """Tests an example entry containing integer data."""
+        """Test an entry containing integer data."""
         valid_integer_entry = ['Category', '5', '2', '6', '873', 'NULL']
         self.assertEqual('INTEGER', decoder.find_type(valid_integer_entry))
 
     def test_real_entry(self):
-        """Tests an example entry containing real number data."""
+        """Test an entry containing real number data."""
         valid_real_entry = ['Category', '8.32', '7.2345', 'NULL', '5']
         self.assertEqual('REAL', decoder.find_type(valid_real_entry))
 
     def test_text_entry(self):
-        """Tests an example entry containing text data."""
+        """Test an example entry containing text data."""
         valid_text_entry = ['Category', '8.32', 'text', '5', 'NULL']
         self.assertEqual('TEXT', decoder.find_type(valid_text_entry))
 
+
 def main():
-    """Launches the unittest main function."""
+    """Launch the unittest main function."""
     unittest.main()
 
 if __name__ == '__main__':
